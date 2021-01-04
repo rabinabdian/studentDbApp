7d34117144440ecb5f114536b1ac9fb81c88c4cc
rabinabdian <rabin.abdian@gmail.com>
2021-01-04 13:21:19 +0200

    student cons

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -5,9 +5,15 @@ public class Student {
     private int stdID;
     private String courses;
     private String tuitionBalsnce;
-    private int costCourse=600;
+    private static int costCourse=600;
+    private static int id = 1000;
 
     // constructor
+
+    public Student(String fN) {
+        this.fN = fN;
+    }
+
 
     // generate id
 

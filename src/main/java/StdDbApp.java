2098a25060e31b6d971df8d0a38240740cab3c36
rabinabdian <rabin.abdian@gmail.com>
2021-01-04 10:23:08 +0200

    student app

---
+++ src/main/java/StdDbApp.java
@@ -0,0 +1,5 @@
+public class StdDbApp {
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/main/java/Student.java
@@ -0,0 +1,24 @@
+public class Student {
+    private String fN;
+    private String lN;
+    private String gradeYear;
+    private int stdID;
+    private String courses;
+    private String tuitionBalsnce;
+    private int costCourse=600;
+
+    // constructor
+
+    // generate id
+
+    // enroll in courses
+
+    //  view balance
+
+    // pay tuition
+
+    // show status
+
+
+
+}
